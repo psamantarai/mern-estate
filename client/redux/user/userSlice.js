9c64bc7c208521445d9de077736041d2c6d8e60d
Pritesh <ksamantarai@gmail.com>
2023-10-13 21:55:17 +0530

    add delete user functionality

--- client/redux/user/userSlice.js
+++ client/redux/user/userSlice.js
@@ -34,6 +34,18 @@ export const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailuer: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -44,6 +56,9 @@ export const {
   updateUserStart,
   updateUserFailuer,
   updateUserSuccess,
+  deleteUserStart,
+  deleteUserFailuer,
+  deleteUserSuccess,
 } = userSlice.actions;
 
 export default userSlice.reducer;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,9 @@ import {
   updateUserStart,
   updateUserFailuer,
   updateUserSuccess,
+  deleteUserStart,
+  deleteUserFailuer,
+  deleteUserSuccess,
 } from "../../redux/user/userSlice";
 
 const Profile = () => {
@@ -79,6 +82,23 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailuer(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailuer(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -140,7 +160,12 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 my-5">{error ? error : ""}</p>
