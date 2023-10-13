8ba94ab50d35dc07256f1a9c021686e6e5196bf0
Pritesh <ksamantarai@gmail.com>
2023-10-13 14:51:42 +0530

    create profile update api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,6 +17,7 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iprcenter.gov%2Fimage-repository%2Fblank-profile-picture.png%2Fimage_view_fullscreen&psig=AOvVaw09F_vlQ_3ai6xC38gaGB5T&ust=1697210321957000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMDtoKTn8IEDFQAAAAAdAAAAABAJ",
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFile%3ADefault_pfp.svg&psig=AOvVaw2-pbXJRcE6cE-SuPhOpuJT&ust=1697275012726000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLikrKPY8oEDFQAAAAAdAAAAABAD"
     },
   },
   { timestamps: true }
