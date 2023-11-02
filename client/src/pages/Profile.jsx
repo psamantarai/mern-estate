6a083ba8161e83f7b5dadac3343987b4ab46b1a3
Pritesh <ksamantarai@gmail.com>
2023-11-03 00:04:23 +0530

    removed console log

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -155,8 +155,6 @@ const Profile = () => {
     }
   };
 
-  console.log(userListings);
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
+      
       if (data.success === false) {
         setLoading(false);
         setError(data.message);
