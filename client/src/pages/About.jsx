b7d85668714885d9ce98bcc89a8facd282078ceb
Pritesh <ksamantarai@gmail.com>
2023-11-02 23:29:05 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Xenon Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At Xenon Estate, we're more than just a property management company –
+        we're your trusted partner in safeguarding and enhancing the value of
+        your real estate investments. With a passion for excellence and a
+        commitment to unparalleled service, we have been serving property owners
+        and investors for over a decade. Our dedicated team of professionals is
+        well-equipped to handle the complexities of estate management, ensuring
+        your properties are not just maintained but thriving.
+      </p>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Our Commitment to Excellence
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At Xenon Estate, we understand that your properties are not just assets;
+        they are the culmination of your dreams and hard work. Our commitment to
+        excellence is rooted in our unwavering dedication to preserving and
+        growing the value of your estate. We leverage our extensive knowledge of
+        the real estate market, cutting-edge technology, and a deep
+        understanding of industry best practices to provide you with top-notch
+        services. Whether you own a single-family home, multi-unit apartment
+        complexes, or commercial properties, our tailored management solutions
+        are designed to meet your unique needs, maximizing your returns and
+        minimizing the hassles.
+      </p>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Your Partner in Estate Success
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Our philosophy is built on the idea that estate management should be
+        seamless, transparent, and stress-free. We take pride in being your
+        partner in estate success, offering services that encompass everything
+        from property maintenance and tenant relations to financial management
+        and market analysis. With Xenon Estate, you can confidently entrust your
+        properties to experienced professionals who share your vision for
+        long-term prosperity. Together, we can transform your real estate
+        investments into thriving assets that stand the test of time.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
